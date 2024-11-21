22ce31b06897c12e456bf39c3d6fc8cffdfdedaa
Muzaffar Soliyev <muzaffar.soliyev97@gmail.com>
2024-11-21 10:25:29 +0500

    added solutions

--- src/main/java/binarySearch/SingleElementInSortedArray.java
+++ src/main/java/binarySearch/SingleElementInSortedArray.java
@@ -3,8 +3,7 @@ package binarySearch;
 public class SingleElementInSortedArray {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        int left = 0;
-        int right = n - 1;
+        int left = 0, right = n - 1;
         if (n == 1) {
             return nums[0];
         }
@@ -19,7 +18,7 @@ public class SingleElementInSortedArray {
             if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
                 return nums[mid];
             }
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
                 left = mid + 1;
             }else {
                 right = mid - 1;
